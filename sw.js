a092336beff372dcc785a1ba2ebc188fca1d5709
Carl Gallardo <carlg@iMacGadd.local>
2025-08-10 05:10:56 +1000

    feat(ui): save FAB+undo, view transitions, persistent storage, maskable icons, swipe tabs; SW v14

--- sw.js
+++ sw.js
@@ -1,6 +1,28 @@
-const CACHE_NAME='wardPWA-v13'; const OLD_CACHES=['wardPWA-v12','wardPWA-v11','wardPWA-v10','wardPWA-v9'];
-self.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./sw.js','./manifest.webmanifest'])))}); 
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([clients.claim(), ...OLD_CACHES.map(name=>caches.delete(name))]))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone(); caches.open(CACHE_NAME).then(c=>c.put(e.request,copy)).catch(()=>{}); return resp}).catch(()=>r)))});f.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open('wardPWA-v9').then(c=>c.addAll(['./','./index.html','./sw.js','./manifest.webmanifest'])))}); 
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open('wardPWA-v14').then(c => c.addAll([
+      './','./index.html','./sw.js','./manifest.webmanifest',
+      './icons/app-192.png','./icons/app-512.png','./icons/maskable-512.png'
+    ]))
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil((async () => {
+    const keep = 'wardPWA-v14';
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k=>k!==keep).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open('wardPWA-v14').then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>r))
+  );
+});f.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open('wardPWA-v9').then(c=>c.addAll(['./','./index.html','./sw.js','./manifest.webmanifest'])))}); 
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim())}); 
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone(); caches.open('wardPWA-v9').then(c=>c.put(e.request,copy)).catch(()=>{}); return resp}).catch(()=>r)))})
